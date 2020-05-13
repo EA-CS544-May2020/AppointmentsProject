8656d582b733aea36328ed438bdbf8394dc97411
Bleink <57928442+Bleink@users.noreply.github.com>
2020-05-13 14:45:57 -0500

    Update Made to Controller

--- src/main/java/cs544/project/controller/AppointmentController.java
+++ src/main/java/cs544/project/controller/AppointmentController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import cs544.project.domain.Appointment;
 import cs544.project.domain.Reservation;
 import cs544.project.service.impl.AppointmentServiceImpl;
-import cs544.project.service.impl.ReservationServiceImpl;
 
 @RestController
 @RequestMapping("/appointments")
@@ -24,21 +22,30 @@ public class AppointmentController {
 	@Autowired
 	private AppointmentServiceImpl appointmentService;
 	
-	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Appointment> getAppointments() {
+		return appointmentService.getAll();
+	}
 
 	@PostMapping
 	public Appointment saveAppointment(@RequestBody Appointment appointment) {
 		return appointmentService.create(appointment);
 	}
 
-	
+	@GetMapping(value = "/{id}")
+	public Appointment getAppointment(@PathVariable Integer id) {
+		return appointmentService.getById(id);
+	}
 
 	@PostMapping(value = "/update")
 	public Appointment updateAppointment(@RequestBody Appointment appointment) {
 		return appointmentService.update(appointment);
 	}
 	
-	
+	@DeleteMapping(value = "/{id}")
+	public void removeAppointment(@PathVariable Integer id) {
+		appointmentService.remove(id);
+	}
 	
 	@GetMapping(value = "/{id}/reservations")
 	public List<Reservation> getReservations(@PathVariable Integer id) {
@@ -50,3 +57,5 @@ public class AppointmentController {
 		return appointmentService.getReservations(id,id2);
 	}
 }
+
+
---
+++ src/main/java/cs544/project/service/impl/AppointmentServiceImpl.java
@@ -0,0 +1,76 @@
+package cs544.project.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cs544.project.domain.Appointment;
+import cs544.project.domain.Reservation;
+import cs544.project.repository.AppointmentRepository;
+import cs544.project.service.AppointmentService;
+
+@Service
+@Transactional
+public class AppointmentServiceImpl implements  AppointmentService{
+	
+	@Autowired
+	private AppointmentRepository appointmentRepo;
+	
+	@Override
+	public List<Appointment> getAll() {
+		return appointmentRepo.findAll();
+	}
+
+	@Override
+	public Appointment getById(Integer id) {
+		Optional<Appointment> appointment = appointmentRepo.findById(id);
+		if (appointment.isPresent()) {
+			return appointment.get();
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Appointment create(Appointment appointment) {
+		return appointmentRepo.save(appointment);
+	}
+
+	@Override
+	public Appointment update(Appointment appointment) {
+		Appointment oldAppointment = getById(appointment.getId());
+		oldAppointment.setDate(appointment.getDate());
+		oldAppointment.setTime(appointment.getTime());
+		oldAppointment.setLocation(appointment.getLocation());
+		oldAppointment.setUser(appointment.getUser());
+		oldAppointment.setReservations(appointment.getReservations());
+		return appointmentRepo.save(oldAppointment);	
+	}
+
+	@Override
+	public void remove(Integer id) {
+		appointmentRepo.deleteById(id);		
+	}
+	
+	@Override
+	public List<Reservation> getReservations(Integer id){
+		Appointment appointment = getById(id);
+		return appointment.getReservations();
+	}
+	
+	@Override
+	public Reservation getReservations(Integer id, Integer id2){
+		Appointment appointment = getById(id);
+		List<Reservation> reservations = appointment.getReservations();
+		Optional<Reservation> reservation= reservations.stream().filter(reserve -> reserve.getId()==id).findAny();
+		if (reservation.isPresent()) {
+			return reservation.get();
+		} else {
+			return null;
+		}
+	}
+}
+
