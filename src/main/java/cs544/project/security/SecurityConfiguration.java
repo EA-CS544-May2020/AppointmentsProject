29c6c3ff112e71cb1c76822920a569b98195c7e4
jpalmat <jimmy.palma1990@gmail.com>
2020-05-13 14:42:33 -0500

    validate login

--- src/main/java/cs544/project/security/SecurityConfiguration.java
+++ src/main/java/cs544/project/security/SecurityConfiguration.java
@@ -5,10 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,18 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
-//Method security
-//@EnableGlobalMethodSecurity(
-////        The prePostEnabled property enables Spring Security pre/post annotations
-//
-//        prePostEnabled = true
-////        The securedEnabled property determines if the @Secured annotation should be enabled
-//
-//        //       securedEnabled = true,
-//
-////        The jsr250Enabled property allows us to use the @RoleAllowed annotation
-//        //       jsr250Enabled = true
-//)
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -38,17 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     //different API have different access requirements
     protected void configure(HttpSecurity http) throws Exception {
-//    	http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER").and().formLogin();
-//    	http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-    	http.authorizeRequests().anyRequest().hasRole("USER").and().httpBasic();
+    	http.authorizeRequests().anyRequest().hasRole("USER").and().formLogin();
     	http.csrf().disable();
 	}
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    	auth.inMemoryAuthentication().withUser("user")
-//        .password("user").roles("USER");
-//    }
     
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) 
@@ -68,7 +45,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
-//    //Method security
-//    public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
-//    }
 }
